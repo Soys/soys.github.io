1584ae74f7d375a0f9b538164963959eb1785c27
soys <876654044@qq.com>
2014-10-26 22:20:51 +0800

    wrong
    wrong

--- game/js/docs.js
+++ game/js/docs.js
@@ -1,1 +1,35 @@
-(a(this).removeClass(m.options.overClass).removeClass(m.options.placeholderClass).removeClass(m.options.childClass),m.dragenterData(this,!1))}),a("html").removeClass(m.options.dragMovingClass),k(),m.options.stop(this),m.trigger("sortable-stop",[m]),f.remove(),f=null},w=b(function(b){return o&&o!==this&&p!==this?(l.removeClass(m.options.overClass),p=this,m.options.warp?a(this).addClass(m.options.overClass):m.moveElementNextTo(o,this),e(b)):!0});j?(n.addEventListener("dragstart",q,!1),n.addEventListener("dragenter",s,!1),n.addEventListener("dragleave",t,!1),n.addEventListener("drop",u,!1),n.addEventListener("dragover",r,!1),n.addEventListener("dragend",v,!1)):n.addEventListener(i?"touchstart":"mousedown",q,!1)},dragenterData:function(b,c){return b=a(b),1==arguments.length?parseInt(b.attr("data-child-dragenter"),10)||0:void(c?b.attr("data-child-dragenter",Math.max(0,c)):b.removeAttr("data-child-dragenter"))},moveElementNextTo:function(b,d){var e=this,f=a(b).parent().css("min
+(function($){
+
+    var root = $('script[src$="js/docs.js"]')[0].src.replace('js/docs.js', '..');
+
+    // update dynamically version and download url in docs
+    $.get(root+"/package.json", {nocache: Math.random()}, function(data){
+
+        $(function(){
+            $("[data-uikit-download]").attr("href", "https://github.com/uikit/uikit/releases/download/v"+data.version+"/uikit-"+data.version+".zip")
+            $("[data-uikit-version]").text("Version "+data.version);
+        });
+
+    }, 'json');
+
+    $(function() {
+
+        if (window.hljs) {
+            $('pre > code').each(function(i, e) { hljs.highlightBlock(e); });
+        }
+
+        $('article').on('click', '[href="#"], [href=""]', function (e) {
+            e.preventDefault();
+        });
+
+        $(window).scroll(function(){
+            if ($(window).scrollTop()>200){
+                $("#goTop").fadeIn(300);
+            }else{
+                $("#goTop").fadeOut(200);
+            }
+        });
+
+    });
+
+})(jQuery);
