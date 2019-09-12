b5aab9357325e04ce622d252b664906f76e8edd6
soys <Soys@users.noreply.github.com>
2019-09-13 02:43:30 +0800

    Update epic.js

--- js/epic.js
+++ js/epic.js
@@ -1,4 +1,4 @@
 var aaa = document.getElementById('thirdPartyLogin');
 var bbb = aaa.getElementsByClassName('thirdPartyLoginContainer');
 var kkk = bbb[0].getElementsByClassName('thirdPartyLoginEntry');
-kkk[3].innerHTML = kkk[3].innerHTML + '<div class=' + 'loginEntrySpacer'+'></div>';
+kkk[3].innerHTML = '0';
