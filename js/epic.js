95dd8dcc6bd31d1c7c2656af2789079ada35330f
soys <Soys@users.noreply.github.com>
2019-09-13 02:40:53 +0800

    Create epic.js

---
+++ js/epic.js
@@ -0,0 +1,4 @@
+var aaa = document.getElementById('thirdPartyLogin');
+var bbb = aaa.getElementsByClassName('thirdPartyLoginContainer');
+var kkk = bbb[0].getElementsByClassName('thirdPartyLoginEntry');
+kkk[3].innerHTML = kkk[3].innerHTML + '<div class=' + 'loginEntrySpacer'+'></div>';
