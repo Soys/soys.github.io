14c35144ec6465644542afc4f8f8df1623b625f2
soys <Soys@users.noreply.github.com>
2024-04-16 03:08:57 +0800

    Create table.js
    for blog

---
+++ js/table.js
@@ -0,0 +1,22 @@
+th, td {
+			  border: 1px solid black;
+			  width: 150px;
+			  height: 50px;
+			  text-align: center;
+}
+.rank {
+				font-weight: bold;
+				font-size: 30px;
+}
+.rank-0 {
+				background-color: green;
+				color: white;
+}
+.rank-18 {
+				background-color: red;
+				color: white;
+}
+#age {
+				color: white;
+				text-decoration: none;
+}
